b817b181ca57f698aae29e9496a3cf4a7833ea95
Vladimir_Zenkov <v.zenkov@coffeestudio.ru>
2025-09-05 22:42:12 +0300

    book font tweaks

--- src/components/record/RecordBook.vue
+++ src/components/record/RecordBook.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="record-book">
+  <div class="record-book" :class="{
+    'record-book_scroll': selectedRecord.data.book_type === 'Scroll',
+    'record-book_book': selectedRecord.data.book_type === 'Book'
+  }">
     <div class="record-book__title">
       {{ selectedRecord.name }}
     </div>
@@ -25,7 +28,6 @@ function parseText(text: String) {
 
 const parsedText = computed(() => parseText(selectedRecord.value.text));
 </script>
-
 <style lang="scss">
 .record-book {
   padding: 20px;
@@ -33,7 +35,7 @@ const parsedText = computed(() => parseText(selectedRecord.value.text));
   margin: 0 auto;
   color: rgb(49, 44, 28);
   &__title {
-    font-size: 25px;
+    font-size: 30px;
     margin-bottom: 20px;
     background-color: rgb(49, 44, 28);
     color: rgb(201, 190, 157);
@@ -42,8 +44,22 @@ const parsedText = computed(() => parseText(selectedRecord.value.text));
     border-radius: 4px;
   }
   &__text {
+    font-size: 25px;
+    width: 47ch;
+    white-space: normal;
+    overflow-wrap: break-word;
+    margin: 0 auto;
     font {
-      font-size: 20px !important;
+      font-size: 25px !important;
+    }
+  }
+  &_scroll {
+    .record-book__text {
+    }
+  }
+  &_book {
+    .record-book__text {
+      width: 29ch;
     }
   }
 }
